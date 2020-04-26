8d238c41b251642a9cfd0df39e76b6ce0597eb73
nlarsen7 <nlarsen2013@gmail.com>
2020-04-26 01:18:46 +0000

    3.6.2 average

--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+//Nicholas Larsen
+//4/25/2020
+//takes scores out of 100 and find the average
 package main
 
 import "fmt"
@@ -7,8 +10,10 @@ func average(scores[]float64)float64{
     total += v
   }
   return total / float64(len(scores))
+  //returns the average
 }
 func main(){
 scores:=[]float64{99,78,87,81,90}
 fmt.Println(average(scores))
+//prints out the average
 }
